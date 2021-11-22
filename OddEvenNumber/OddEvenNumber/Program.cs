624108cb2679382d6cc3a10d64285e27354c898f
yusuf kenan girgin <yusufkenangirgin@hotmail.com>
2021-11-22 10:15:45 +0300

    1

---
+++ OddEvenNumber/OddEvenNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OddEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 % 2);
+            Console.WriteLine(6 % 3);
+            Console.WriteLine(10 % 4);Console.WriteLine(10 % 4);Console.WriteLine(10 % 4);
+        }
+    }
+}
