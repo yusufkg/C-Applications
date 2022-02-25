6cdcd35e5e6cd7c9c739017335cde533e4e1a2d2
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2022-02-25 03:54:42 +0300

    v1

---
+++ IngılızceHarflerLoops/IngılızceHarflerLoops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngılızceHarflerLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A, B, C, D ... Z
+            for (int i = 65; i <= 90; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+            Console.WriteLine();
+            for (char c = 'A'; c <= 'z'; c++)
+            {
+                if(c <= 'Z' || c >= 'a')
+                Console.Write(c + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
