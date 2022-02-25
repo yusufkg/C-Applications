bf6e1fa8f4605a31d85fb70941f0a47b40bdb4d2
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2022-02-25 03:56:34 +0300

    v1

---
+++ Methods/CalculatorDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1. sayi: ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("2. sayı: ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İşlem (+, -, *, /): ");
+            string islem = Console.ReadLine();
+            double islemSonucu = IslemYap(sayi1, sayi2, islem);
+            Console.WriteLine("Sonuç: " + islemSonucu);
+            Console.ReadLine();
+        }
+
+        static double IslemYap(double sayi1, double sayi2, string islem)
+        {
+            double sonuc;
+            if (islem == "+")
+                sonuc = sayi1 + sayi2;
+            else if (islem == "-")
+                sonuc = sayi1 - sayi1;
+            else if (islem == "*")
+                sonuc = sayi1 * sayi2;
+            else // islem == "/"
+                sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+    }
+}
